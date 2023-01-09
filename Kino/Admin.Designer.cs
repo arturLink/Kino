30b9f2f7cdc663da97a018325b5cc54959fe50ac
vafle <vafle@DESKTOP-D6239CC>
2023-01-09 22:24:46 +0200

    working

--- Kino/Admin.Designer.cs
+++ Kino/Admin.Designer.cs
@@ -28,19 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.filmiPlt = new System.Windows.Forms.PictureBox();
+            this.nimiBox = new System.Windows.Forms.TextBox();
+            this.aastaBox = new System.Windows.Forms.TextBox();
+            this.zanrBox = new System.Windows.Forms.TextBox();
+            this.keelBox = new System.Windows.Forms.TextBox();
+            this.kestusBox = new System.Windows.Forms.TextBox();
+            this.uuendaBtn = new System.Windows.Forms.Button();
+            this.kustutaBtn = new System.Windows.Forms.Button();
+            this.liisaBtn = new System.Windows.Forms.Button();
+            this.piltBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filmiPlt)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(31, 378);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1176, 251);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+            // 
+            // filmiPlt
+            // 
+            this.filmiPlt.Location = new System.Drawing.Point(929, 24);
+            this.filmiPlt.Name = "filmiPlt";
+            this.filmiPlt.Size = new System.Drawing.Size(278, 330);
+            this.filmiPlt.TabIndex = 1;
+            this.filmiPlt.TabStop = false;
+            // 
+            // nimiBox
+            // 
+            this.nimiBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.nimiBox.Location = new System.Drawing.Point(684, 24);
+            this.nimiBox.Name = "nimiBox";
+            this.nimiBox.Size = new System.Drawing.Size(163, 39);
+            this.nimiBox.TabIndex = 2;
+            // 
+            // aastaBox
+            // 
+            this.aastaBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.aastaBox.Location = new System.Drawing.Point(684, 99);
+            this.aastaBox.Name = "aastaBox";
+            this.aastaBox.Size = new System.Drawing.Size(163, 39);
+            this.aastaBox.TabIndex = 3;
+            // 
+            // zanrBox
+            // 
+            this.zanrBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.zanrBox.Location = new System.Drawing.Point(684, 174);
+            this.zanrBox.Name = "zanrBox";
+            this.zanrBox.Size = new System.Drawing.Size(163, 39);
+            this.zanrBox.TabIndex = 4;
+            // 
+            // keelBox
+            // 
+            this.keelBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.keelBox.Location = new System.Drawing.Point(684, 246);
+            this.keelBox.Name = "keelBox";
+            this.keelBox.Size = new System.Drawing.Size(163, 39);
+            this.keelBox.TabIndex = 5;
+            // 
+            // kestusBox
+            // 
+            this.kestusBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.kestusBox.Location = new System.Drawing.Point(684, 315);
+            this.kestusBox.Name = "kestusBox";
+            this.kestusBox.Size = new System.Drawing.Size(163, 39);
+            this.kestusBox.TabIndex = 6;
+            // 
+            // uuendaBtn
+            // 
+            this.uuendaBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.uuendaBtn.Location = new System.Drawing.Point(185, 300);
+            this.uuendaBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uuendaBtn.Name = "uuendaBtn";
+            this.uuendaBtn.Size = new System.Drawing.Size(129, 43);
+            this.uuendaBtn.TabIndex = 17;
+            this.uuendaBtn.Text = "Uuenda";
+            this.uuendaBtn.UseVisualStyleBackColor = true;
+            this.uuendaBtn.Click += new System.EventHandler(this.uuendaBtn_Click);
+            // 
+            // kustutaBtn
+            // 
+            this.kustutaBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.kustutaBtn.Location = new System.Drawing.Point(31, 300);
+            this.kustutaBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.kustutaBtn.Name = "kustutaBtn";
+            this.kustutaBtn.Size = new System.Drawing.Size(129, 43);
+            this.kustutaBtn.TabIndex = 16;
+            this.kustutaBtn.Text = "Kustuta";
+            this.kustutaBtn.UseVisualStyleBackColor = true;
+            this.kustutaBtn.Click += new System.EventHandler(this.kustutaBtn_Click);
+            // 
+            // liisaBtn
+            // 
+            this.liisaBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.liisaBtn.Location = new System.Drawing.Point(337, 300);
+            this.liisaBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.liisaBtn.Name = "liisaBtn";
+            this.liisaBtn.Size = new System.Drawing.Size(129, 43);
+            this.liisaBtn.TabIndex = 15;
+            this.liisaBtn.Text = "Liisa";
+            this.liisaBtn.UseVisualStyleBackColor = true;
+            this.liisaBtn.Click += new System.EventHandler(this.liisaBtn_Click);
+            // 
+            // piltBtn
+            // 
+            this.piltBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.piltBtn.Location = new System.Drawing.Point(491, 300);
+            this.piltBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.piltBtn.Name = "piltBtn";
+            this.piltBtn.Size = new System.Drawing.Size(129, 43);
+            this.piltBtn.TabIndex = 14;
+            this.piltBtn.Text = "Otsi fail";
+            this.piltBtn.UseVisualStyleBackColor = true;
+            this.piltBtn.Click += new System.EventHandler(this.piltBtn_Click);
             // 
             // Admin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1258, 664);
+            this.Controls.Add(this.uuendaBtn);
+            this.Controls.Add(this.kustutaBtn);
+            this.Controls.Add(this.liisaBtn);
+            this.Controls.Add(this.piltBtn);
+            this.Controls.Add(this.kestusBox);
+            this.Controls.Add(this.keelBox);
+            this.Controls.Add(this.zanrBox);
+            this.Controls.Add(this.aastaBox);
+            this.Controls.Add(this.nimiBox);
+            this.Controls.Add(this.filmiPlt);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Admin";
             this.Text = "Admin";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filmiPlt)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox filmiPlt;
+        private System.Windows.Forms.TextBox nimiBox;
+        private System.Windows.Forms.TextBox aastaBox;
+        private System.Windows.Forms.TextBox zanrBox;
+        private System.Windows.Forms.TextBox keelBox;
+        private System.Windows.Forms.TextBox kestusBox;
+        private System.Windows.Forms.Button uuendaBtn;
+        private System.Windows.Forms.Button kustutaBtn;
+        private System.Windows.Forms.Button liisaBtn;
+        private System.Windows.Forms.Button piltBtn;
     }
 }
--- Kino/Admin.cs
+++ Kino/Admin.cs
@@ -2,19 +2,123 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ScrollBar;
 
 namespace Kino
 {
     public partial class Admin : Form
     {
+        SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\vafle\source\repos\Kino\Kino\KinoDB.mdf;Integrated Security=True");
+        SqlDataAdapter adapter_filmid = new SqlDataAdapter();
+        SqlCommand cmd;
         public Admin()
         {
             InitializeComponent();
+            Naita_Andmed();
+        }
+
+        public void Naita_Andmed()
+        {
+            connect.Open();
+            DataTable dt_toode = new DataTable();
+            //cmd = new SqlCommand("SELECT * FROM Toodetable");
+            adapter_filmid = new SqlDataAdapter("SELECT * FROM Filmid", connect);
+            adapter_filmid.Fill(dt_toode);
+            dataGridView1.DataSource = dt_toode;
+
+
+            filmiPlt.Image = Image.FromFile("../../ProjectImages/Info.jpg");
+            filmiPlt.SizeMode = PictureBoxSizeMode.StretchImage;
+            connect.Close();
+            //Naita_Kategooria();
+
+        }
+        public void Kustuta_Andmed()
+        {
+            nimiBox.Text = "";
+            aastaBox.Text = "";
+            zanrBox.Text = "";
+            keelBox.Text = "";
+            kestusBox.Text = "";
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            nimiBox.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            aastaBox.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            zanrBox.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            keelBox.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+            kestusBox.Text = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+            try
+            {
+                filmiPlt.Image = Image.FromFile(@"..\..\MovieImages\" + dataGridView1.Rows[e.RowIndex].Cells[6].Value.ToString());
+            }
+            catch (Exception)
+            {
+                filmiPlt.Image = Image.FromFile(@"..\..\images\Info.png");
+                MessageBox.Show("Fail puudub");
+            }
+        }
+
+        private void kustutaBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uuendaBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void liisaBtn_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Kas unustasite pildi valida?", "Hoiatus!",MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+            if (result == DialogResult.No)
+            {
+                FileInfo info = new FileInfo(filmiPlt.ImageLocation.ToString());
+                string pltnimi = info.Name;
+                if (nimiBox.Text.Trim() != string.Empty && aastaBox.Text.Trim() != string.Empty &&
+                    zanrBox.Text.Trim() != string.Empty && keelBox.Text.Trim() != string.Empty && kestusBox.Text.Trim() != string.Empty)
+                {
+                    try
+                    {
+                        cmd = new SqlCommand("INSERT INTO Filmid (Nimi,Aasta,Zanr,Keel,Kestus,Pilt)" +
+                            " VALUES (@nimi,@aasta,@zanr,@keel,@kestus,@pilt)", connect); //@pilt, //Pilt
+                        connect.Open();
+                        cmd.Parameters.AddWithValue("@nimi", nimiBox.Text);
+                        cmd.Parameters.AddWithValue("@aasta", aastaBox.Text);
+                        cmd.Parameters.AddWithValue("@zanr", zanrBox.Text);
+                        cmd.Parameters.AddWithValue("@keel", keelBox.Text);
+                        cmd.Parameters.AddWithValue("@kestus", kestusBox.Text);
+                        cmd.Parameters.AddWithValue("@pilt", pltnimi);
+                        cmd.ExecuteNonQuery();
+                        connect.Close();
+                        Kustuta_Andmed();
+                        Naita_Andmed();
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Andmeebaasi viga");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Sissesta korektselt andmeid");
+                }
+            }
+        }
+
+        private void piltBtn_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
